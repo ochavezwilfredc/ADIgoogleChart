d7e93e93ee58ced7422e04c7c8127f3b4743150e
ochavezwilfredc <ochavezwilfredc@crece.uss.edu.pe>
2019-02-24 22:34:12 -0500

    cambios

--- resources/getdata.php
+++ resources/getdata.php
@@ -1,19 +1,24 @@
 <?php 
 // MySQL database connection code
-$connection = mysqli_connect("localhost","root","","dbslim") or die("Error " . mysqli_error($connection));
+$connection = mysqli_connect("localhost","root","","dbgraficosadi") or die("Error " . mysqli_error($connection));
+
 //Fetch productos data
 $sql = "SELECT * FROM alumno";
+
 $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+
 //create an array
 $array = array();
+
 $i = 0;
-while($row = mysqli_fetch_assoc($result))
-{  
+while($row = mysqli_fetch_assoc($result)){  
     $producto = $row['nombre'];
     $unidades_vendidas = $row['calificacion'];
     $array['cols'][] = array('type' => 'string'); 
     $array['rows'][] = array('c' => array( array('v'=> $producto), array('v'=>(int)$unidades_vendidas)) );
 }
+
 $data = json_encode($array);
+
 echo $data;
 ?>
